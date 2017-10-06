71ff9139d8f0b4d4c98240a6ecbcfe69f8933cd7
tlornet <tlornet@teclib.com>
2017-10-06 10:43:52 +0200

    thread ok but blocking Karaf console

--- orgdevtomosgiblueprint/src/main/java/org/devtom/osgi/blueprint/example/controller/MyTimeServiceConsumer.java
+++ orgdevtomosgiblueprint/src/main/java/org/devtom/osgi/blueprint/example/controller/MyTimeServiceConsumer.java
@@ -5,6 +5,7 @@ import org.devtom.osgi.timeservice.ITimeService;
 public class MyTimeServiceConsumer implements IMyTimeServiceConsumer {
 
     private ITimeService timeService;
+    private TimeServiceThread thread;
 
     public MyTimeServiceConsumer(ITimeService tService) {
         timeService = tService;
@@ -27,5 +28,43 @@ public class MyTimeServiceConsumer implements IMyTimeServiceConsumer {
 
     public void setTimeService(ITimeService timeService) {
         this.timeService = timeService;
+    }
+
+    public void startUp() {
+        System.out.println("MyTimeServiceConsumer startUp()");
+        thread = new TimeServiceThread(timeService);
+        thread.run();
+    }
+
+    public void stopThread() {
+        thread.stopThread();
+    }
+
+    public static class TimeServiceThread extends Thread {
+
+        private volatile boolean active = true;
+        private final ITimeService timeService;
+
+        public TimeServiceThread(ITimeService tService) {
+            this.timeService = tService;
+        }
+
+        public void run() {
+            while (active) {
+                try {
+                    Thread.sleep(5000);
+                    if (active) {
+                        System.out.println("TimeServiceThread: uh!");
+                        System.out.println("TimeServiceThread: " + timeService.getCurrentTime());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Thread interrupted " + e.getMessage());
+                }
+            }
+        }
+
+        public void stopThread() {
+            active = false;
+        }
     }
 }
